684738bb000fb6107d2b823055eefb3ee90ccf75
Thijs Dijkwel <thijsdijkwel@gmail.com>
2018-12-22 01:58:51 -0600

    project to help me pack for Semester at Sea

--- packing.py
+++ packing.py
@@ -2,3 +2,4 @@
 # Figure out a way to randomly generate one thing every time I hit enter.
 # I pack whatever pops up. After it pops up, I enter True or False for whether I packed it or not.
 # If True, have the item be taken off of the list. If False, have the item be shuffled back in.
+print ('lets go')
