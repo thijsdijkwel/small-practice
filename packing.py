b121b750b3e21c7f8d0e4938486426f309d12792
Thijs Dijkwel <thijsdijkwel@gmail.com>
2018-12-22 16:05:12 -0600

    updated list

--- packing.py
+++ packing.py
@@ -3,9 +3,12 @@
 # I pack whatever pops up. After it pops up, I enter True or False for whether I packed it or not.
 # If True, have the item be taken off of the list. If False, have the item be shuffled back in.
 import random
-clothes = (['undergarments', 'pants','tshirts','hoodie','sweater','jacket','rain jacket','shoes','watch','hats', 'scarf', 'swimwear','formal'])
-electronics = (['camera', 'USB', 'charging adapter', 'headphones', 'flashlight', 'gopro', 'macbook'])
-toiletries = (['shampoo', 'conditioner', 'toothbrush', 'toothpaste', 'mouthwash', 'deoderant', 'comb', 'pomade', 'contacts', 'contact solution', 'glasses', 'razor'])
+clothes = (['undergarments', 'pants','tshirts','hoodie','sweater','jacket','rain jacket','shoes','watch', 'belts','hats', 'scarf', 'swimwear','formal'])
+electronics = (['camera', 'USB', 'charging adapter', 'headphones', 'flashlight', 'gopro', 'macbook', 'calculator',])
+toiletries = (['shampoo', 'conditioner', 'toothbrush', 'toothpaste', 'mouthwash', 'deoderant', 'comb', 'pomade', 'anti-wrinkle spray','contacts', 'contact solution', 'glasses', 'razor'])
 medical = (['prescriptions', 'tylenol', 'melatonin', 'first aid kid',])
-print clothes + electronics + toiletries + medical
-print (len(clothes + electronics + toiletries + medical))
+miscellaneous = (['credit cards', 'debit cards', 'cash', 'water bottle', 'beach towel', 'thank you cards', 'packing organizer', 'laundry', 'snacks'])
+everything = clothes + electronics + toiletries + medical + miscellaneous
+
+
+print random.choice(everything)
