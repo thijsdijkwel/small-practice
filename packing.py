7483786b9428b3b8278134eb91107035c714462c
Thijs Dijkwel <thijsdijkwel@gmail.com>
2018-12-22 13:04:41 -0600

    created lists

--- packing.py
+++ packing.py
@@ -2,4 +2,10 @@
 # Figure out a way to randomly generate one thing every time I hit enter.
 # I pack whatever pops up. After it pops up, I enter True or False for whether I packed it or not.
 # If True, have the item be taken off of the list. If False, have the item be shuffled back in.
-print ('lets go')
+import random
+clothes = (['undergarments', 'pants','tshirts','hoodie','sweater','jacket','rain jacket','shoes','watch','hats', 'scarf', 'swimwear','formal'])
+electronics = (['camera', 'USB', 'charging adapter', 'headphones', 'flashlight', 'gopro', 'macbook'])
+toiletries = (['shampoo', 'conditioner', 'toothbrush', 'toothpaste', 'mouthwash', 'deoderant', 'comb', 'pomade', 'contacts', 'contact solution', 'glasses', 'razor'])
+medical = (['prescriptions', 'tylenol', 'melatonin', 'first aid kid',])
+print clothes + electronics + toiletries + medical
+print (len(clothes + electronics + toiletries + medical))
