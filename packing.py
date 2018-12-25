eecd8d56d61e1d0e7c8f504969023293963ea3bb
Thijs Dijkwel <thijsdijkwel@gmail.com>
2018-12-24 23:36:09 -0600

    trying to figure out def function

--- packing.py
+++ packing.py
@@ -1,4 +1,4 @@
-# Create a list of all of the things I need to pack for SaS.
+
 # Figure out a way to randomly generate one thing every time I hit enter.
 # I pack whatever pops up. After it pops up, I enter True or False for whether I packed it or not.
 # If True, have the item be taken off of the list. If False, have the item be shuffled back in.
